d0fab68e3a7fff0f5589b81b468616af71a70ea6
Alicia Muniz <alicia.muniz@gestaopublica.etc.br>
2025-01-20 16:34:48 -0300

    implementa funcao para agregar amigos e acao no botao

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+
+// Adicionar nomes: Os usuários escreverão o nome de um amigo em um campo de texto e o adicionarão a uma lista visível ao clicar em "Adicionar".
+function adicionarAmigo() {
+    let amigo = document.querySelector('input').value;
+    if (amigo == '') {
+        alert('Insira um nome válido');
+    } else {
+        listaDeAmigos.push(amigo);
+        // Limpa o campo de entrada
+        document.querySelector('input').value = '';
+    }
+}
+// Adiciona os eventos aos botões
+document.getElementById('button-add').addEventListener('click', () => {
+    listarAmigos('listaAmigos');
+});
+
